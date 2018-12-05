2e5a57ebc494fdc42bc4d075d0436be466f26950
lmy <13400355215@163.com>
2018-12-05 08:53:06 +0800

    提交

--- src/main/java/com/neuedu/controller/TestController.java
+++ src/main/java/com/neuedu/controller/TestController.java
@@ -1,4 +1,18 @@
 package com.neuedu.controller;
 
+import com.neuedu.pojo.UserInfo;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/login/user")
 public class TestController {
+
+    @RequestMapping(value = "/login.do")
+    public UserInfo login(){
+        UserInfo userInfo=new UserInfo();
+        userInfo.setId(1);
+        userInfo.setUsername("zhangsan");
+        return userInfo;
+    }
 }
